fe2e2f7ba078e8ddb7a6e8400030aa3296eecc0b
maxoganian <62914182+maxoganian@users.noreply.github.com>
2020-04-04 13:56:48 -0400

    New code as of 4-4-20

--- nrf24_reliable_datagram_receiver.ino
+++ nrf24_reliable_datagram_receiver.ino
@@ -18,9 +18,29 @@
 #define SERVER_ADDRESS 2
 
 uint8_t wireData[7]; 
-int ledPin = 6;
-byte potPin = A0;
+byte potPin = A15;
 int potValue = 0;
+//variables for led pins
+int blueLedPin = 30;
+int greenLedPin = 31;
+int redLedPin = 32;
+//variable for the number of moisture sensors
+int numMoistSensors = 2;
+//define sensor pins
+byte sensorPins[] = {A0, A1, A2};
+//define pin the button is plugged into
+int buttonPin[] = {28,29};
+//Boolean for wether or not the button is pressed
+int buttonPressed[] = {0, 0};
+//variable for the value that a plant becomes to low at
+int tooDryValue[] = {0, 0};
+//variable for the current readings of the sensor
+int currentMoisture[] = {10, 10};
+//boolean to tell us if a variable is to low
+boolean isTooDry = false;
+//int for the value of the light sensor
+int lightValue = 0;
+
 // Singleton instance of the radio driver
 RH_NRF24 driver;
 // RH_NRF24 driver(8, 7);   // For RFM73 on Anarduino Mini
@@ -29,7 +49,7 @@ RH_NRF24 driver;
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
 //setup LCD pins
-LiquidCrystal lcd(9,7,5,4,3,2);
+LiquidCrystal lcd(22,23,24,25,26,27);
 
 void setup() 
 {
@@ -40,8 +60,10 @@ void setup()
   lcd.begin(16, 2);
   //clear LCD display
   lcd.clear();
-  //led pin
-  pinMode(ledPin, OUTPUT);
+  //led pins initillization
+  pinMode(blueLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
   //potentiometer pin
   pinMode(potPin, INPUT);
   if (!manager.init()){
@@ -58,6 +80,108 @@ uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
 
 void loop()
 {
+  potValue = analogRead(potPin);
+  lightValue = map(analogRead(sensorPins[0]), 0, 1028, 100, 0);
+  if(lightValue > 100)
+    lightValue=0;
+  //see if a button has been pressed
+  buttonPressed[0] = digitalRead(buttonPin[0]);
+  buttonPressed[1] = digitalRead(buttonPin[1]);
+  //map the readings we get from the moisture sensor to something we can send
+  for(int k=0;k<=numMoistSensors;k++){
+    currentMoisture[k] = map(analogRead(sensorPins[k+1]), 0, 1028, 0, 100);
+  }
+  //if the button is pressed set a new to dry value
+  //for(int m=0;m<=numMoistSensors; m++){
+  if(digitalRead(buttonPin[0]) == HIGH){
+    tooDryValue[0] = currentMoisture[0];
+  }
+  if(buttonPressed[1] == HIGH){
+    tooDryValue[1] = currentMoisture[1];
+  }
+  //}
+  //if the current moistre of the plant is below a threshhold then we are to dry
+  //for(int l=0;l<=numMoistSensors;l++){
+  if(currentMoisture[0] < tooDryValue[0] || currentMoisture[1] < tooDryValue[1]){
+    isTooDry = true;
+  }
+  else{
+    isTooDry = false;
+  }
+  Serial.print("Light: ");
+  Serial.println(lightValue);
+  Serial.print("isTooDry: ");
+  Serial.println(isTooDry);
+  Serial.print("Moisture: ");
+  Serial.println(currentMoisture[0]);
+  Serial.print("To Dry Value: ");
+  Serial.println(tooDryValue[0]);
+  Serial.print("Moisture2: ");
+  Serial.println(currentMoisture[1]);
+  Serial.print("To Dry Value2: ");
+  Serial.println(currentMoisture[1]);
+  Serial.print("Button 1: ");
+  Serial.println(digitalRead(buttonPin[0]));
+  Serial.print("Button 2: ");
+  Serial.println(buttonPressed[1]);
+ 
+  //print values to LCD display
+  if(potValue < 360){
+    lcd.setCursor(0,0);
+    lcd.print("M1 Sun: H20: TDV:");
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(3,1);
+    lcd.print(lightValue);
+    lcd.setCursor(8,1);
+    lcd.print(currentMoisture[0]);
+    lcd.setCursor(13,1);
+    lcd.print(tooDryValue[0]);
+  }
+  if(potValue > 360 && potValue < 720){
+    lcd.setCursor(0,0);
+    lcd.print("M2 Sun: H20: TDV:");
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(3,1);
+    lcd.print(lightValue);
+    lcd.setCursor(8,1);
+    lcd.print(currentMoisture[1]);
+    lcd.setCursor(13,1);
+    lcd.print(tooDryValue[1]);
+  }
+  if(isTooDry)
+    digitalWrite(redLedPin, HIGH);
+  else
+    digitalWrite(redLedPin, LOW);
+  wireData[0] = lightValue;
+  wireData[1] = isTooDry;
+  wireData[2] = currentMoisture[0]; 
+  wireData[3] = tooDryValue[0];
+  wireData[4] = currentMoisture[1];
+  wireData[5] = tooDryValue[1];
+  wireData[6] = 99;
+  if(wireData[0]==99)
+    wireData[0]=98;
+  if(wireData[1]==99)
+    wireData[1]=98;
+  if(wireData[2]==99)
+    wireData[2]=98;
+  if(wireData[3]==99)
+    wireData[3]=98;
+  if(wireData[4]==99)
+    wireData[4]=98;
+  if(wireData[5]==99)
+    wireData[5]=98;
+   
+  for(int i=0;i<=6;i++){
+    //Serial.print("sending wire ");
+    //Serial.println(i);
+    Wire.beginTransmission(9);
+    Wire.write(wireData[i]);
+    delay(100);
+    Wire.endTransmission();
+  }
   //Serial.println("Sending");
   if (manager.available())
   {
@@ -66,78 +190,7 @@ void loop()
     uint8_t from;
     if (manager.recvfromAck(buf, &len, &from))
     {
-      potValue = analogRead(potPin);
-      Serial.print("got request from : 0x");
-      Serial.println(from, HEX);
-      Serial.print("Light: ");
-      Serial.println(buf[0]);
-      Serial.print("isTooDry: ");
-      Serial.println(buf[1]);
-      Serial.print("Moisture: ");
-      Serial.println(buf[2]);
-      Serial.print("To Dry Value: ");
-      Serial.println(buf[3]);
-      Serial.print("Moisture2: ");
-      Serial.println(buf[4]);
-      Serial.print("To Dry Value2: ");
-      Serial.println(buf[5]);
-      //print values to LCD display
-      if(potValue < 360){
-        lcd.setCursor(0,0);
-        lcd.print("M1 Sun: H20: TDV:");
-        lcd.setCursor(0,1);
-        lcd.print("                ");
-        lcd.setCursor(3,1);
-        lcd.print(buf[0]);
-        lcd.setCursor(8,1);
-        lcd.print(buf[2]);
-        lcd.setCursor(13,1);
-        lcd.print(buf[3]);
-      }
-      if(potValue > 360 && potValue < 720){
-        lcd.setCursor(0,0);
-        lcd.print("M2 Sun: H20: TDV:");
-        lcd.setCursor(0,1);
-        lcd.print("                ");
-        lcd.setCursor(3,1);
-        lcd.print(buf[0]);
-        lcd.setCursor(8,1);
-        lcd.print(buf[4]);
-        lcd.setCursor(13,1);
-        lcd.print(buf[5]);
-      }
-      if(buf[1])
-        digitalWrite(ledPin, HIGH);
-      else
-        digitalWrite(ledPin, LOW);
-      wireData[0] = buf[0];
-      wireData[1] = buf[1];
-      wireData[2] = buf[2]; 
-      wireData[3] = buf[3];
-      wireData[4] = buf[4];
-      wireData[5] = buf[5];
-      wireData[6] = 99;
-      if(wireData[0]==99)
-        wireData[0]=98;
-      if(wireData[1]==99)
-        wireData[1]=98;
-      if(wireData[2]==99)
-        wireData[2]=98;
-      if(wireData[3]==99)
-        wireData[3]=98;
-      if(wireData[4]==99)
-        wireData[4]=98;
-      if(wireData[5]==99)
-        wireData[5]=98;
-      
-      for(int i=0;i<=6;i++){
-        Serial.print("sending wire ");
-        Serial.println(i);
-        Wire.beginTransmission(9);
-        Wire.write(wireData[i]);
-        delay(100);
-        Wire.endTransmission();
-      } 
+       
       // Send a reply back to the originator client
       if (!manager.sendtoWait(data, sizeof(data), from))
         Serial.println("sendtoWait failed");
