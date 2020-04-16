e6971357f9dc1452acc1813fbf0121e050105192
maxoganian <62914182+maxoganian@users.noreply.github.com>
2020-04-16 13:08:59 -0400

    Add files via upload

--- wire_receive.ino
+++ wire_receive.ino
@@ -14,13 +14,36 @@
 #include <SPI.h>
 #include <SD.h>
 
+#include <SparkFunDS1307RTC.h>
+
 const int chipSelect = 10;
 unsigned long timeSinceStart = 0;
+
+#define SQW_INPUT_PIN 2   // Input pin to read SQW
+#define SQW_OUTPUT_PIN 13 // LED to indicate SQW's state
+
 void setup()
 {
-  Wire.begin(9);                // join i2c bus with address #4
-  Wire.onReceive(receiveEvent); // register event
+  Wire.begin();                // join i2c bus as master
   Serial.begin(9600);           // start serial for output
+
+  Serial.begin(9600);
+  
+  pinMode(SQW_INPUT_PIN, INPUT_PULLUP);
+  pinMode(SQW_OUTPUT_PIN, OUTPUT);
+  digitalWrite(SQW_OUTPUT_PIN, digitalRead(SQW_INPUT_PIN));
+  
+  rtc.begin(); // Call rtc.begin() to initialize the library
+  // (Optional) Sets the SQW output to a 1Hz square wave.
+  // (Pull-up resistor is required to use the SQW pin.)
+  rtc.writeSQW(SQW_SQUARE_1);
+
+  // Now set the time...
+  // You can use the autoTime() function to set the RTC's clock and
+  // date to the compiliers predefined time. (It'll be a few seconds
+  // behind, but close!)
+  rtc.autoTime();
+  rtc.set12Hour(); // Use rtc.set12Hour to set to 12-hour mode
   
   Serial.print("Initializing SD card...");
 
@@ -33,82 +56,86 @@ void setup()
   Serial.println("card initialized.");
 }
 int i = 0;
+int data[8];
 void loop()
 {
-  delay(100);
-}
-int data[7];
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-void receiveEvent(int howMany)
-{
-    timeSinceStart = millis()/1000;
+    Wire.requestFrom(9,64);
     
-    data[i] = Wire.read(); // receive byte as a character
-    //.print("0");
-    //Serial.println(i);
-    // make a string for assembling the data to log:
-    String dataString = "";
-  
-    // read three sensors and append to the string:
-    dataString += String(data[i]);
-     // open the file. note that only one file can be open at a time,
-    // so you have to close this one before opening another.
-    File dataFile = SD.open("datalog5.txt", FILE_WRITE);
-  
-    // if the file is available, write to it:
-    if (dataFile) {
-      if(i == 0){
-        dataFile.print("Seconds since start: ");
-        dataFile.println(timeSinceStart);
-        Serial.print("Seconds since start: ");
-        Serial.println(timeSinceStart);
+    while(Wire.available()){
+      data[i] = Wire.read(); // receive byte as a character
+      //.print("0");
+      //Serial.println(i);
+      // make a string for assembling the data to log:
+      String dataString = "";
+    
+      // read three sensors and append to the string:
+      dataString += String(data[i]);
        
-        dataFile.print("Light: ");
-        Serial.print("Light: ");
+      // if the file is available, write to it:
+      if(data[i]  < 100){
+        // open the file. note that only one file can be open at a time,
+        // so you have to close this one before opening another.
+        File dataFile = SD.open("windtest.txt", FILE_WRITE);
+      
+        if (dataFile) {
+          if(i == 0){           
+            dataFile.print("Light: ");
+            Serial.print("Light: ");
+          }
+          if(i == 1){
+            dataFile.print("isTooDry: ");
+            Serial.print("isTooDry: ");    
+          }
+          if(i == 2){
+            dataFile.print("Moisture 1: ");
+            Serial.print("Moisture 1: ");
+          }
+          if(i == 3){
+            dataFile.print("Too Dry Value 1: ");
+            Serial.print("Too Dry Value 1: ");
+          }
+          if(i == 4){
+            dataFile.print("Moisture 2: ");
+            Serial.print("Moisture 2: ");
+          }
+          if(i == 5){
+            dataFile.print("Too Dry Value 2: ");
+            Serial.print("Too Dry Value 2: ");
+          }
+          if(i == 6){
+            dataFile.print("Wind Reading: ");
+            Serial.print("Wind Reading: ");
+          }
+          if(i == 7){
+            dataFile.println("------------");
+            Serial.println("------------");
+          }      
+          if(i != 7){
+            dataFile.println(dataString);
+            Serial.println(dataString);         // print the character
+          }  
+          Serial.print("i = ");
+          Serial.println(i);
+          dataFile.close();
+        }
+        // if the file isn't open, pop up an error:
+        else {
+          Serial.println("error opening datalog.txt");
+        }
       }
-      if(i == 1){
-        dataFile.print("isTooDry: ");
-        Serial.print("isTooDry: ");    
+      if(data[i]==99){
+        i=7;
       }
-      if(i == 2){
-        dataFile.print("Moisture 1: ");
-        Serial.print("Moisture 1: ");
+      i++;
+      if(i>7){ 
+        //Serial.println("Reseting i");
+        i = 0;
       }
-      if(i == 3){
-        dataFile.print("Too Dry Value 1: ");
-        Serial.print("Too Dry Value 1: ");
-      }
-      if(i == 4){
-        dataFile.print("Moisture 2: ");
-        Serial.print("Moisture 2: ");
-      }
-      if(i == 5){
-        dataFile.print("Too Dry Value 2: ");
-        Serial.print("Too Dry Value 2: ");
-      }
-      if(i == 6){
-        dataFile.println("------------");
-        Serial.println("------------");
-      }      
-      if(i != 6){
-        dataFile.println(dataString);
-        Serial.println(dataString);         // print the character
-      }  
-//      Serial.print("i = ");
-//      Serial.println(i);
-      dataFile.close();
-    }
-    // if the file isn't open, pop up an error:
-    else {
-      Serial.println("error opening datalog.txt");
-    }
+    //delay(100);
+  }
+}
 
-    if(data[i]==99){
-      i=6;
-    }
-    i++;
-    if(i>6){ 
-      i = 0;
-    }
+void printTime()
+{ 
+  
 }
